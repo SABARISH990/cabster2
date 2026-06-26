5352cc4289fdacb3fbd82a3471a66475430a7d65
SABARISH990 <sabarish5763@gmail.com>
2026-06-26 20:39:07 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import Loader from "./components/Loader";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./pages/About";
